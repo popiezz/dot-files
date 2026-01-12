12d6e4edc1dde2e25430757832a191e5c8251e71
popiezz <philpopescu@outlook.com>
2026-01-12 08:47:45 -0500

    added tokyonight

--- lua/phil/plugins/colorscheme.lua
+++ lua/phil/plugins/colorscheme.lua
@@ -7,12 +7,13 @@ return {
 	{ "blazkowolf/gruber-darker.nvim", priority = 1000 },
 	{ "f4z3r/gruvbox-material.nvim", priority = 1000 },
 	{ "rebelot/kanagawa.nvim", priority = 1000 },
+	{ "folke/tokyonight.nvim", priority = 1000 },
 	{
 		"whatyouhide/vim-gotham",
 		priority = 1000,
 		lazy = false,
 		config = function()
-			vim.cmd.colorscheme("kanagawa")
+			vim.cmd.colorscheme("tokyonight")
 		end,
 	},
 }
