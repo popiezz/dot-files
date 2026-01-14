affeb32632681599c6f82726babdeba4110e2911
popiezz <philpopescu@outlook.com>
2026-01-14 08:21:04 -0500

    added nordic theme

--- lua/phil/plugins/colorscheme.lua
+++ lua/phil/plugins/colorscheme.lua
@@ -8,12 +8,13 @@ return {
 	{ "f4z3r/gruvbox-material.nvim", priority = 1000 },
 	{ "rebelot/kanagawa.nvim", priority = 1000 },
 	{ "folke/tokyonight.nvim", priority = 1000 },
+	{ "AlexvZyl/nordic.nvim", priority = 1000 },
 	{
 		"whatyouhide/vim-gotham",
 		priority = 1000,
 		lazy = false,
 		config = function()
-			vim.cmd.colorscheme("tokyonight")
+			vim.cmd.colorscheme("nordic")
 		end,
 	},
 }
