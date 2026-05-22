3dbe2352fb99583d4fe775842950c3a3da5cad4c
Filip <philpopescu@gmail.com>
2026-05-22 13:12:59 -0400

    added black-metal theme

--- lua/phil/plugins/auto-dark-mode.lua
+++ lua/phil/plugins/auto-dark-mode.lua
@@ -4,12 +4,12 @@ return {
 		update_interval = 1000,
 		set_dark_mode = function()
 			vim.api.nvim_set_option_value("background", "dark", {})
-			vim.cmd("colorscheme rose-pine")
+			vim.cmd("colorscheme black-metal")
 		end,
 		set_light_mode = function()
 			vim.api.nvim_set_option_value("background", "dark", {})
 			-- Choose a light-compatible theme from your installed list
-			vim.cmd("colorscheme rose-pine")
+			vim.cmd("colorscheme black-metal")
 		end,
 	},
 }
--- lua/phil/plugins/colorscheme.lua
+++ lua/phil/plugins/colorscheme.lua
@@ -21,4 +21,17 @@ return {
 			},
 		},
 	},
+	{
+		"metalelf0/black-metal-theme-neovim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("black-metal").setup({
+				theme = "bathory",
+				variant = "dark",
+				alt_bg = false,
+			})
+			require("black-metal").load()
+		end,
+	},
 }
