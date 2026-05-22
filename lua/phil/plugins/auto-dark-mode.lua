da91d06c1ead6c63994212cb57b20a19f1e4ca63
Filip <philpopescu@gmail.com>
2026-05-22 13:22:33 -0400

    fixed black-metal theme load

--- lua/phil/plugins/auto-dark-mode.lua
+++ lua/phil/plugins/auto-dark-mode.lua
@@ -1,15 +1,17 @@
 return {
 	"f-person/auto-dark-mode.nvim",
+	dependencies = {
+		"metalelf0/black-metal-theme-neovim",
+	},
 	opts = {
 		update_interval = 1000,
 		set_dark_mode = function()
 			vim.api.nvim_set_option_value("background", "dark", {})
-			vim.cmd("colorscheme black-metal")
+			require("black-metal").load() -- ✅ use the plugin's own API
 		end,
 		set_light_mode = function()
 			vim.api.nvim_set_option_value("background", "dark", {})
-			-- Choose a light-compatible theme from your installed list
-			vim.cmd("colorscheme black-metal")
+			require("black-metal").load()
 		end,
 	},
 }
